ab5787645031615c3faa2a91c69f644c597a2703
janny <2431349655@qq.com>
2020-12-28 16:26:54 +0800

    修改端口号

--- public/app.js
+++ public/app.js
@@ -3,6 +3,6 @@ const serve = require('koa-static');
 
 const app = new Koa();
 app.use(serve(__dirname + "/public")); //将public下的代码静态化
-app.listen(3333, () => {
+app.listen(9527, () => {
      console.log('人资项目启动')
 })
